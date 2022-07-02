3b99586afe5c0895f3443d7e32b6b4c6bae02c17
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-07-02 04:05:37 +0100

    1_square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Representation of a square"""
+
+    def __init__(self, size):
+        """Initialize a square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
