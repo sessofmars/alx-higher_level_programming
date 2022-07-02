ba88ac18a1d8babef607122acb189ef5ff14ffb8
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-07-02 04:15:44 +0100

    update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Representation of a square"""
+
+    def __init__(self, size=0):
+        """Initialize a square
+        Args:
+            size (int): size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/Set size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Return area of the square."""
+        return self.__size * self.__size
