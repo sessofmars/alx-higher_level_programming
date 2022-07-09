5c5944a69a24817c1694c9ecdca61cf2a21dac32
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-07-09 04:49:11 +0100

    defines an empty class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
