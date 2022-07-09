033ae706f1957820ee54296ebc578ff232f33906
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-07-09 04:51:45 +0100

    writes a class

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates an integer value
+        Args:
+            name (str): name of the parameter to validate
+            value (int): parameter to validate
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is <= 0
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+
+        if value < 1:
+            raise ValueError("{} must be greater than 0".format(name))
