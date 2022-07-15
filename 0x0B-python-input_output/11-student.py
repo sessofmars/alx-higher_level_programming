61069308f00dd1893c4d87d280fa8ee43592ee6d
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-07-15 02:06:29 +0100

    retrieve a dictionary pointer

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""Defines a Student class"""
+
+
+class Student:
+    """A Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student object
+        Args:
+            first_name (str): first name of the student
+            last_name (str): last name of the student
+            age (int): age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student
+        If attrs is a list of strings, only attribute names
+        contained in this list are retrieved.
+        Args:
+            attrs (list): attributes to retrieve
+        """
+        if type(attrs) == list:
+            if all(type(elem) == str for elem in attrs):
+                return {key: getattr(self, key)
+                        for key in attrs if hasattr(self, key)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of Student instance
+        Args:
+            json (dict): key-value pair to replace attributes with
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
