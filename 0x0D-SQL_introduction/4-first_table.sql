5d1553210c73e4154d031cdf2605f078a1a3435c
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-08-16 16:42:52 +0100

    creates a table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
 -- A script that creates a table called first_table.
-CREATE TABLE IF NOT EXISTS first_table(
-	`id` INT,
-	`name` VARCHAR(256),
-	)
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT,`name` VARCHAR(256))
