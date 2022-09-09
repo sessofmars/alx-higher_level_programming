3294f38eb341395231701b1d10d890334451c5a9
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-09-09 04:39:01 +0100

    all

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const myVar = 'C is fun'\n;'Python is cool'\n;'JavaScript is amazing'\n;
-console.log(myVar);
+const myVar = 'C is fun';
+const myVar1 = 'Python is cool';
+const myVar2 = 'JavaScript is amazing';
+const all = `${myVar}${myVar1}${myVar2}`;
+console.log(all);
