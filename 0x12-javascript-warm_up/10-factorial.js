4f7283503b1a8be3943ccf2dc192c2cce36a8214
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-09-11 03:45:19 +0100

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+
+function factorial (num) {
+  if (num < 0) return;
+  if (isNaN(num) || num === 0) return 1;
+  return num * factorial(num - 1);
+}
+console.log(factorial(num));
