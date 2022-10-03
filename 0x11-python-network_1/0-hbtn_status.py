bc47818f3d81575b885f7ab7025aa5467bfd7081
sessofmars <99624302+sessofmars@users.noreply.github.com>
2022-10-03 04:47:29 +0100

    fetches a url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
-import urllib.request
+"""A script that
+- fetches https://intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-req = urllib.request.Request('http://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(req) as response:
-   the_page = response.read()
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
